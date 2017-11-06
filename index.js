8851577e5847a677cfc7b7092ec7595e07e455ba
Elliot Kim <elliotkim916@gmail.com>
2017-11-06 12:58:09 -0800

    First Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const STORE = [
+    {
+        question: "What is the name of Tom Haverford's alcoholic beverage?",
+        answers: ["Lemon Sexy", "Tommy Fresh", "Snake Juice", "Cobra Venom" ],
+        correctAnswer: "Snake Juice"
+    },
+    {
+        question: "What kind of car does Donna drive?",
+        answers: ["Jaguar", "Rolls Royce", "Mercedes Benz", "Ferrari"],
+        correctAnswer: "Mercedes Benz"
+    },
+    {
+        question: "What is Andy Dwyer's alter ego?",
+        answers: ["Burt Macklin, FBI", "Randy Handler, CIA", "Jack Thrifty, DEA", "Paula Hart, PTA"],
+        correctAnswer: "Bert Macklin, FBI"
+    },
+    {
+        question: "Where was Leslie Knopp born?",
+        answers: ["Eagleton", "Pawnee", "Indianapolis", "Washington D.C."],
+        correctAnswer: "Eagleton"
+    },
+    {
+        question: "What is Ben's terrible choice of favorite food?",
+        answers: ["Mini muffins", "Calzones", "Pop-Tarts", "Hot-Pockets"],
+        correctAnswer: "Calzones"
+    },
+    {
+        question: "Who is Leslie Knopp's teenage nemesis?",
+        answers: ["Gary Finklestein", "Greg Pikitus", "George Patticus", "Ginny Weasley"],
+        correctAnswer: "Greg Pikitus"
+    }
+];
